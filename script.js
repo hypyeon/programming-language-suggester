8673509fca110136b9946e8f5894e4ba51c2a879
Hayeong Pyeon <hayeong@HayeongcBookAir.home>
2023-11-02 14:17:58 -0700

    add next button function - debugging atm

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+function toNext() {
+    let currentCardIndex = 0;
+    const cards = document.getElementsByClassName('card');
+    let next = document.getElementsByClassName('nextBtn');
 
+    next.addEventListener('click', function() {
+        cards[currentCardIndex].style.display = 'none';
+        currentCardIndex++;
+        if (currentCardIndex >= cards.length) {
+            currentCardIndex = 0;
+        }
+        cards[currentCardIndex].style.display = 'block';
+    });
+    cards[0].style.display = 'block';
+};
 
+window.onload = function() {
+    toNext();
+}
