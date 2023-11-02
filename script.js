4314ce75f668bfac85c273edcd360171f170f59f
Hayeong Pyeon <hayeong@HayeongcBookAir.home>
2023-11-02 15:56:05 -0700

    add flex on scss

--- script.js
+++ script.js
@@ -15,15 +15,38 @@ function toNext() {
     });
 
     next = document.getElementById('toCard2');
-    next.addEventListener('click', function() {
+    next.addEventListener('click', function(e) {
+        e.preventDefault();
         card2.style.display = 'block';
         card1.style.display = 'none';
     });
 
     next = document.getElementById('toCard3');
-    next.addEventListener('click', function() {
+    next.addEventListener('click', function(e) {
+        e.preventDefault();
         card3.style.display = 'block';
         card2.style.display = 'none';
+    });
+
+    next = document.getElementById('toCard4');
+    next.addEventListener('click', function(e) {
+        e.preventDefault();
+        card4.style.display = 'block';
+        card3.style.display = 'none';
+    });
+
+    next = document.getElementById('toCard5');
+    next.addEventListener('click', function(e) {
+        e.preventDefault();
+        card5.style.display = 'block';
+        card4.style.display = 'none';
+    });
+
+    next = document.getElementById('toResult');
+    next.addEventListener('click', function(e) {
+        e.preventDefault();
+        result.style.display = 'block';
+        card5.style.display = 'none';
     });
 };
 
